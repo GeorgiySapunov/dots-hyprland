f3926148da5c4a928adcd635d6eed3c5abf4b6c0
Georgiy Sapunov <georgiysapunov@yandex.ru>
2023-01-28 15:39:42 +0300

    mounter update

--- nvim/.config/nvim/lua/user/barbaric.lua
+++
@@ -1,2 +0,0 @@
--- The IME to invoke for managing input languages (macos, fcitx, ibus, xkb-switch)
-vim.cmd([[let g:barbaric_ime = 'ibus']])
--- nvim/.config/nvim/lua/user/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-local colorscheme = "catppuccin"
--- require("catppuccin").setup()
-
-local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
-if not status_ok then
-  return
-end
